c8230864073606bbb3644725c32fac13b586cd66
Kerr Ivan Agapito Cirilo <kerr.ivan29@gmail.com>
2018-07-27 20:35:26 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,34 @@
+
+
+for (var i = 1; i< 100;i++) {
+  console.log ("Hello World the" + i + "time");
+}
+
+function forLoop(array){
+  for(let i=0; i<25;i++){
+    if (i===1){
+      array.push(`I am 1 strange loop.`)
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array;
+}
+function maybeTrue(){
+  return Math.random() >= 0.5 
+}
+
+let countdowm = 100;
+while (countdown > 0) {
+  console.log(--countdown)
+}
+
+function whileLoop(n){
+ 
+  while (n > 0) {
+    console.log (--n)
+  }
+  return 'done'
+}
+
+function doWhileLoop() 
