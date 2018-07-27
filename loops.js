09bf5ea77fe1e046eb070c095b44fc5349abd480
Kerr Ivan Agapito Cirilo <kerr.ivan29@gmail.com>
2018-07-27 21:20:59 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -20,7 +20,7 @@ function doWhileLoop(array){
   function maybeTrue() {
     return Math.random () >=0.5 
       }
-  do (array.pop()){
+  do {array.pop()
     
   } while (array.length>0 || maybeTrue());
 }
